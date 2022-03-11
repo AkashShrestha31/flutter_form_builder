569b4887cf5c1ebf93b45046d99e7d73c606512b
Raju Gotame <munnadroid@Rajus-MacBook-Pro.local>
2022-03-11 16:47:04 +0545

    Chang en text

--- packages/form_builder_validators/lib/localization/l10n.dart
+++ packages/form_builder_validators/lib/localization/l10n.dart
@@ -101,10 +101,10 @@ class FormBuilderLocalizations {
     );
   }
 
-  /// `This field requires a valid email address.`
+  /// `This field requires a valid xyz address.`
   String get emailErrorText {
     return Intl.message(
-      'This field requires a valid email address.',
+      'This field requires a valid xyz address.',
       name: 'emailErrorText',
       desc: 'Error Text for email field',
       args: [],
